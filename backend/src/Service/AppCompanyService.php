411fafb6bc49acba5fb0c869b65bed9dac680b60
ioedumac <eacevedof@yahoo.es>
2020-01-25 18:41:50 +0100

    mejora insert

--- backend/src/Service/AppCompanyService.php
+++ backend/src/Service/AppCompanyService.php
@@ -1,9 +1,6 @@
 <?php
 namespace App\Service;
-
 use App\Service\BaseService;
-
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 use App\Entity\AppCompany;
 use App\Form\SignupType as SigunpForm;
@@ -16,32 +13,33 @@ final class AppCompanyService extends BaseService
     
     public function insert()
     {
-        $container = $this->arinput["container"];
+        /**
+         * @var Symfony\Component\HttpFoundation\Request
+         */
         $request = $this->arinput["request"];
+        //@var Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+        $container = $this->arinput["container"];
+        //@var Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
         $encoder = $this->arinput["encoder"];
+        
         
         $company = new AppCompany();
         $form = $container->get("form.factory")->create(SigunpForm::class, $company);
         $form->handleRequest($request);
-        //$form = $this->createForm(SignupType::class,$company);
-        
        
         if($form->isSubmitted() && $form->isValid())
         {
-
-            $company->setRole("ROLE_USER");
             $passencoded = $encoder->encodePassword($company,$company->getPassword());
             $company->setPassword($passencoded);
+            $company->setRole("ROLE_USER");
             
-            $em = $container->get('doctrine')->getManager();
+            $em = $container->get("doctrine")->getManager();
             $em->persist($company);
-
             $em->flush();
-            
-            //return $this->redirectToRoute("pub_index");
+
             $this->aroutput["inserted"] = true;
         }
-
+        
         $this->aroutput["form"] = $form;
     }
     
